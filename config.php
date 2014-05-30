74c5390f032994bdf2bcc63abd0af5c8e5ccc342
Danny Cheok <danny@gimmieworld.com>
2014-05-30 11:51:31 +0800

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ define("FACEBOOK_SECRET", "<FACEBOOK_SECRET>"); //Create a Facebook app and get 
 define("COMPANY_NAME", "<COMPANY NAME>"); //A name for the company
 define("LOCALE_CODE", "en_US"); //Locale for the reward content
 define("COUNTRY_CODE", "global"); //Set the reward country (global mean it will detect user IP to list reward. *this can slow down the page)
-define("GIMMIE_REFERRER_EVENT", "did_fanpage_referral"); //Create the event "did_fanpage_referral" for your Game in Gimmie (trigger when user refer someone to join. Set frequency to 1)
-define("GIMMIE_LIKE_EVENT", "did_fanpage_like"); //Create the event "did_fanpage_like" for your Game in Gimmie (trigger when LIKE fan page. Set frequency to 1)
+define("GIMMIE_REFERRER_EVENT", "refer_a_friend"); //Create the event "refer_a_friend" for your Game in Gimmie (trigger when user refer someone to join. Set frequency to 1)
+define("GIMMIE_LIKE_EVENT", "like_fanpage"); //Create the event "like_fanpage" for your Game in Gimmie (trigger when LIKE fan page. Set frequency to 1)
 
 ?>
